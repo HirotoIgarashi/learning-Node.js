95d7ba8ac1a779ff5a821c47c6314d0aa7c9b347
Hiroto Igarashi <hirotoigarashi@gmail.com>
2024-09-27 16:32:30 +0900

    update package.json

---
+++ myapp/mochaFileWrite.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+// import fs from "fs";
+
+describe("fs", () => {
+  describe("writeFile", () => {
+    it("エラーなしで実行できる", (done) => {
+      fs.writeFile("hello.txt", "Hello world", (err) =>
+        err ? done(err) : done(),
+      );
+      // またはよりシンプルに
+      // fs.writeFile('hello.txt', 'Hello world', done);
+    });
+  });
+});
