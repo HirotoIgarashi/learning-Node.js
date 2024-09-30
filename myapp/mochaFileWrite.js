4891a8ae123db98b3175f4cfaa8622e0777d28b2
Hiroto Igarashi <hirotoigarashi@gmail.com>
2024-09-30 18:48:54 +0900

    add .editorconfig update js file

--- myapp/mochaFileWrite.js
+++ myapp/mochaFileWrite.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 // import fs from "fs";
 
 describe("fs", () => {
-  describe("writeFile", () => {
-    it("エラーなしで実行できる", (done) => {
-      fs.writeFile("hello.txt", "Hello world", (err) =>
-        err ? done(err) : done(),
-      );
-      // またはよりシンプルに
-      // fs.writeFile('hello.txt', 'Hello world', done);
+    describe("writeFile", () => {
+        it("エラーなしで実行できる", (done) => {
+            fs.writeFile("hello.txt", "Hello world", (err) =>
+                err ? done(err) : done(),
+            );
+            // またはよりシンプルに
+            // fs.writeFile('hello.txt', 'Hello world', done);
+        });
     });
-  });
 });
