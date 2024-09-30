574e8c61ea05919889246b8042810829c93cbd28
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-09-30 21:03:16 +0900

    delete eslintrc.json add eslintrc.mjs update package.json

---
+++ eslintrc.mjs
@@ -0,0 +1,41 @@
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...compat.extends("plugin:react/recommended", "plugin:@typescript-eslint/recommended"),
+    {
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 5,
+            sourceType: "script",
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            "react/prop-types": "off",
+            "@typescript-eslint/no-empty-interface": 0,
+        },
+    },
+];
