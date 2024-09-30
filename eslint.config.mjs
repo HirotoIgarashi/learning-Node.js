c51cb8ee01379f90c3fa7890dd60735e3d75c3e0
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-09-30 21:31:13 +0900

    update package.json and eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,9 @@
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [
-    ...compat.extends("plugin:react/recommended", "plugin:@typescript-eslint/recommended"),
-    {
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 5,
-            sourceType: "script",
-
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-        },
-
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-
-        rules: {
-            "react/prop-types": "off",
-            "@typescript-eslint/no-empty-interface": 0,
-        },
-    },
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
 ];
