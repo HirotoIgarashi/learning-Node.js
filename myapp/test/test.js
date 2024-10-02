0b04bf454a4f57342c38b5d7457a49e856bdd57e
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-10-02 23:38:16 +0900

    update

--- myapp/test/test.js
+++ myapp/test/test.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const predicate = require("../lib/predicate");
+const assert = require("assert");
 
 describe("fs", () => {
     describe("writeFile", () => {
@@ -9,5 +11,10 @@ describe("fs", () => {
             // またはよりシンプルに
             // fs.writeFile('hello.txt', 'Hello world', done);
         });
+    });
+});
+describe("predicate", () => {
+    it("1 + 2で3を返す", () => {
+        assert.equal(predicate.add(1, 2), 3);
     });
 });
