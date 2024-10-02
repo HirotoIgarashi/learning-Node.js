402ae8bbb5ec11db9425b989b66496a5973b66c8
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-10-02 17:33:35 +0900

    add hello.txt test.mjs

---
+++ myapp/lib/test.mjs
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+describe("fs", () => {
+    describe("writeFile", () => {
+        it("エラーなしで実行できる", (done) => {
+            fs.writeFile("hello.txt", "Hello world", (err) =>
+                err ? done(err) : done(),
+            );
+            // またはよりシンプルに
+            // fs.writeFile('hello.txt', 'Hello world', done);
+        });
+    });
+});
