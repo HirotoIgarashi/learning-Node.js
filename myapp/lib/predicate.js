0c7afaa3eab0432ac35432ebe363e5358b8de9c7
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-10-04 03:05:01 +0900

    update predicate.js test.js

--- myapp/lib/predicate.js
+++ myapp/lib/predicate.js
@@ -1,3 +1,54 @@
-module.exports.lessOrEqual = (x, y) => {
-    return x <= y;
+module.exports.lessOrEqual = (x, y) => x <= y;
+module.exports.isEqual = (x, y) => {
+    return x === y;
+};
+module.exports.isEmpty = (o) => {
+    return o;
+};
+module.exports.isElement = (o) => {
+    return o;
+};
+module.exports.isArray = (o) => Array.isArray(o);
+module.exports.isObject = (obj) => typeof obj == "object";
+module.exports.isArguments = (x, y) => {
+    return x + y;
+};
+module.exports.isFunction = (fn) =>
+    Object.prototype.toString.call(fn) === "[object Function]";
+module.exports.isString = (x, y) => {
+    return x + y;
+};
+module.exports.isNumber = (o) => {
+    return o;
+};
+// isFinite関数は引数が有限数かどうかを判定します
+module.exports.isFinite = (o) => isFinite(o);
+module.exports.isBoolean = (o) => {
+    return o;
+};
+module.exports.isDate = (o) => {
+    return o;
+};
+// 日本の郵便番号かどうか。xxx-xxxxを期待している。
+// candidateは候補者の意
+// ToDo: xxx-xxxxでもxxxxxxxでも受け入れられるように改造する。
+module.exports.isZipCodeJp = (candidate) => {
+    if (typeof candidate !== "string" || candidate.length != 8) {
+        return false;
+    }
+    return /^\d{3}-\d{4}$/.test(candidate);
+};
+module.exports.isRegExp = (o) => {
+    return o;
+};
+module.exports.isNaN = (o) => isNaN(o);
+module.exports.isNull = (o) => {
+    if (o == null) {
+        return true;
+    } else {
+        return false;
+    }
+};
+module.exports.isUndefined = (o) => {
+    return o;
 };
--- myapp/test/test.js
+++ myapp/test/test.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
-const predicate = require("../lib/predicate");
+const lessOrEqual = require("../lib/predicate").lessOrEqual;
+const isArray = require("../lib/predicate").isArray;
+const isObject = require("../lib/predicate").isObject;
+const isFunction = require("../lib/predicate").isFunction;
+const isFinite = require("../lib/predicate").isFinite;
+const isZipCodeJp = require("../lib/predicate").isZipCodeJp;
+const isNaN = require("../lib/predicate").isNaN;
+const isNull = require("../lib/predicate").isNull;
 const assert = require("assert");
+const testFunction = () => 0;
+function Ninja() {}
+var ninja = new Ninja();
 
 describe("fs", () => {
     describe("writeFile", () => {
@@ -14,13 +24,49 @@ describe("fs", () => {
     });
 });
 describe("predicate", () => {
-    it("数値を比較する", () => {
-        assert.equal(predicate.lessOrEqual(1, 2), true);
+    it("より小さいかイコールか 1 < 2なのでtrue", () => {
+        assert.equal(lessOrEqual(1, 2), true);
     });
-    it("数値を比較する", () => {
-        assert.equal(predicate.lessOrEqual(2, 1), false);
+    it("より小さいかイコールか 2 < 1なのでfalse", () => {
+        assert.equal(lessOrEqual(2, 1), false);
     });
-    it("数値を比較する", () => {
-        assert.equal(predicate.lessOrEqual(1, 1), true);
+    it("より小さいかイコールか 1 = 1なのでtrue", () => {
+        assert.equal(lessOrEqual(1, 1), true);
+    });
+    it("[]が配列(Array)かどうか", () => {
+        assert.equal(isArray([]), true);
+    });
+    it("{}が配列(Array)かどうか", () => {
+        assert.equal(isArray({}), false);
+    });
+    it("オブジェクトかどうか", () => {
+        assert.equal(isObject(ninja), true);
+    });
+    it("関数かどうか", () => {
+        assert.equal(isFunction(testFunction), true);
+    });
+    it("有限数かどうか", () => {
+        assert.equal(isFinite(1), true);
+    });
+    it("有限数かどうか", () => {
+        assert.equal(isFinite(NaN), false);
+    });
+    it("郵便番号かどうか", () => {
+        assert.equal(isZipCodeJp("135-0004"), true);
+    });
+    it("NaNが不定値(NaN(not-a-number)かどうか", () => {
+        assert.equal(isNaN(NaN), true);
+    });
+    it("nullがnullかどうか", () => {
+        assert.equal(isNull(null), true);
+    });
+    it("0がnullかどうか", () => {
+        assert.equal(isNull(0), false);
+    });
+    it("Number.NaNが不定値(NaN(not-a-number)かどうか", () => {
+        assert.equal(isNaN(Number.NaN), true);
+    });
+    it("0/0が不定値(NaN(not-a-number)かどうか", () => {
+        assert.equal(isNaN(0 / 0), true);
     });
 });
